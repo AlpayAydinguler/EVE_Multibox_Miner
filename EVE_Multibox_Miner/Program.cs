2801ef6dd9c7548015c5ca84d4bbd5f47c093030
Alpay Aydıngüler <alpayaydinguler@gmail.com>
2025-07-09 04:06:22 +0300

    Form1 deletion fix

--- EVE_Multibox_Miner/Program.cs
+++ EVE_Multibox_Miner/Program.cs
@@ -16,7 +16,7 @@ namespace EVE_Multibox_Miner
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
     }
 }
